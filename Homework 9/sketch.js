734d018714b15f98bda11b557cfa56ce119d72fb
NathanKornick <nathan.kornick@umontana.edu>
2022-03-24 13:49:45 -0600

    Homework 9
    Hey Professor Cassens,
    
    For my portrait, I ended up going in a bit of a different direction and did a portrait of just my face. I still used all of the shapes and functions that you listed out in the homework, so hopefully that's okay. Thanks!

---
+++ Homework 9/sketch.js
@@ -0,0 +1,49 @@
+function setup () 
+{
+    createCanvas(600,500);
+}
+
+function draw () 
+{
+    stroke('black')
+    background (220);
+    strokeWeight (1);
+    fill (255,230,80);
+    circle (250,250,500);
+    fill ('white')
+    ellipse (150,150,80,40);
+    ellipse (350,150,80,40);
+    fill (100,149,237);
+    circle (150,150,40);
+    circle (350,150,40);
+    fill (255,230,80);
+    triangle (200,315,250,150,300,315);
+    fill (250,128,114)
+    ellipse (250,370,200,35);
+    line (250,370,150,370);
+    line (250,370,350,370);
+    strokeWeight (20);
+    point (150,150);
+    point (350,150);
+    strokeWeight (1);
+    fill (255,230,80);
+    ellipse (0,200,50,125);
+    ellipse (500,200,50,125);
+    fill (186,74,0)
+    rect(105,112,80,13);
+    rect(315,112,80,13);
+    ellipse (250,40,500,80);
+    ellipse (30,90,100,130);
+    ellipse (470,90,100,130);
+    textSize(25);
+    fill ('black');
+    text ('Nathan Kornick',420,495);
+    stroke(231,76,60);
+    strokeWeight (10);
+    point (260,140);
+    point (340,250);
+    point (140,340);
+    point (330,400);
+    point (95,120);
+    point (415,120);
+}
