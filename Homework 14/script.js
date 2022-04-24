26f4fea8ad97c68966641f5863fe69a3b056821e
NathanKornick <nathan.kornick@umontana.edu>
2022-04-24 15:37:12 -0600

    Homework 14

---
+++ Homework 14/script.js
@@ -0,0 +1,186 @@
+var characterX = 20;
+var characterY = 560;
+
+var w = 87;
+var s = 83;
+var a = 65;
+var d = 68;
+
+var shape1X = 30;
+var shape1Y = 50;
+var shape2X = 470;
+var shape2Y = 550;
+var shapeXSpeed;
+var shapeYSpeed;
+var shape2XSpeed;
+var shape2YSpeed;
+
+var mouseShapeX;
+var mouseShapeY;
+
+var mousex = -10;
+var mousey = -10;
+
+var rectangleObject;
+var rectangleObject2;
+var rectangleObject3;
+
+function setup()
+{
+    createCanvas(500,600);
+
+    //create the object
+    rectangleObject = new Rectangle(100,200,10,50,120,35,210);
+    rectangleObject2 = new Rectangle(320,70,100,30,30,400,88);
+    rectangleObject3 = new Rectangle(250,375,40,60,600,125,496);
+
+    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shape2XSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 2);
+    shape2YSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 2);
+
+
+}
+
+function draw()
+{
+    background(75,13,13);
+    stroke(0);
+    fill(0);
+
+    //create and display object
+    rectangleObject.display();
+    rectangleObject2.display();
+    rectangleObject3.display();
+    
+    //calling the functions
+    createBorders();
+    createCharacter();
+    characterMovement();
+    createObstacles();
+    bounceShapes();
+    createMouseShapes();
+    createExitMessage();
+
+//defining the functions
+function createBorders()
+{
+ // top border
+ fill(164,155,155)
+ rect(0,0,width,10);
+ // left border
+ rect(0,0,10,height);
+ // bottom border
+ rect(0,height-10,width,10);
+ // right upper border
+ rect(width-10,0,10,height-500);
+ //right upper borderB
+ rect(width-10,height-440,10,440)
+}
+
+function createCharacter()
+{
+ //character
+ fill(164,155,155);
+ square(characterX,characterY,20);
+}
+
+function characterMovement()
+{
+    if(keyIsDown(w))
+    {
+        characterY -= 10;
+    }
+        if(keyIsDown(s))
+    {
+        characterY += 10;
+    }
+        if(keyIsDown(a))
+    {
+        characterX -= 10;
+    }
+        if(keyIsDown(d))
+    {
+        characterX += 10;
+    }
+}
+
+function createObstacles()
+{
+// obstacle1
+fill(19,34,127)
+circle(shape1X,shape1Y,20);
+shape1X += shapeXSpeed;
+shape1Y += shapeYSpeed;
+//obstacle2
+fill(215,225,52);
+circle(shape2X,shape2Y,40);
+shape2X -= shape2XSpeed;
+shape2Y -= shape2YSpeed;
+}
+
+function bounceShapes()
+{
+// bounceShape1
+if(shape1X > width)
+{
+    shape1X = 0;
+}
+if(shape1X < 0)
+{
+    shape1X = width;
+}
+if(shape1Y > height)
+{
+    shape1Y = 0;
+}
+if(shape1Y < 0)
+{
+    shape1Y = height;
+}
+// bounceShape2
+if(shape2X > width)
+{
+    shape2X = 0;
+}
+if(shape2X < 0)
+{
+    shape2X = width;
+}
+if(shape2Y > height)
+{
+    shape2Y = 0;
+}
+if(shape2Y < 0)
+{
+    shape2Y = height;
+}
+}
+
+function createMouseShapes()
+{
+// mouse1
+fill(254,254,254)
+circle(mousex,mousey,30);
+}
+
+function createExitMessage()
+{
+// crossing the exit
+if(characterX > width && characterY > width-420 && characterY < width-340)
+{
+    fill('white');
+    stroke(5);
+    textSize(26);
+    text("You Win!", width/2-50, height/2-50);
+}
+}
+}
+
+// mouse2
+function mouseClicked()
+{
+    mousex = mouseX;
+    mousey = mouseY;
+}
+
